7afe711b9c22c33d67338c8c108bfdbb26fd9e08
alexeiscript <suzakukururug1@yandex.com>
2020-12-08 10:14:48 +0200

    creating components for home section

---
+++ src/components/skills.js
@@ -0,0 +1,14 @@
+import { computeHeadingLevel } from '@testing-library/react';
+import React, {Component} from 'react';
+
+class Skills extends Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Skills;
