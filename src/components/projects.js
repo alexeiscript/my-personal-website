b1678bc6f11f458e1d354c1eef10b663f0f38beb
alexeiscript <suzakukururug1@yandex.com>
2021-04-01 14:08:35 +0100

    updating projects links

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,8 +3,8 @@ import '../styles/projects.css';
 
 function Projects() {
     const myWorks = [
-        {id: 1, title: 'Dostoyevsky Quotes', description: 'A quote generator built with JavaScript, which randomly iterates through an array of quotes', img: 'http://alexei.one/dostoyevsky-quotes/dstvsk.png', link: 'https://alexei.one/dostoyevsky-quotes/'},
-        {id: 2, title: 'Matching Poster Game', description: 'A game built with JavaScript, involving DOM manipulation and conditionals', img: 'http://alexei.one/matching-game/matching-game.png', link: 'https://alexei.one/matching-game/'},
+        {id: 1, title: 'Dostoyevsky Quotes', description: 'A quote generator built with JavaScript, which randomly iterates through an array of quotes', img: 'http://alexei.one/dostoyevsky-quotes/dstvsk.png', link: 'https://dostoyevsky-quotes.netlify.app/'},
+        {id: 2, title: 'Matching Poster Game', description: 'A game built with JavaScript, involving DOM manipulation and conditionals', img: 'http://alexei.one/matching-game/matching-game.png', link: 'https://poster-match.netlify.app/'},
         {id: 3, title: 'Calculator', description: 'A basic working calculator built with React as part of an online JavaScript course', img: 'https://images.freeimages.com/images/large-previews/916/calculator-and-pen-indicating-work-study-1632106.jpg', link: 'https://alexei.one/calculator/'},
         {id: 4, title: 'Plant Health Care', description: 'I developed this website during my time at Design Portfolio as Junior Developer', img: 'https://ar18.planthealthcare.com/application/files/6615/5240/5747/Image.jpg', link: 'https://ar18.planthealthcare.com'},
         {id: 5, title: 'J Cargo LTD', description: 'A website I built for a London based removals and delivery business, with pure JavaScript', img: 'http://jcargo.co.uk/jcargoImage.png', link: 'https://jcargo.co.uk'},
